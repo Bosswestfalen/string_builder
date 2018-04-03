471d5d0c7b5e9c17e1537e8b089dbcb8f384d402
Bosswestfalen <24429152+Bosswestfalen@users.noreply.github.com>
2018-04-03 23:07:11 +0200

    :heavy_minus_sign: Removed cmake_minimum_required in script

--- cmake/wipe_doc.cmake
+++ cmake/wipe_doc.cmake
@@ -3,9 +3,6 @@
 # This script wipes the generated html files, so that no zombie-files remain in the git repo
 # ToDo:
 # - is there a doxygen command for that? (Did not find one so far)
-# - is cmake_minimum_required required?
-
-cmake_minimum_required(VERSION 3.9)
 
 file(REMOVE_RECURSE "${CMAKE_SOURCE_DIR}/../doc/html")
 
